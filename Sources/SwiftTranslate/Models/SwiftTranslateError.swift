45dff18511581ffd6dff93673f30ac76e854614d
Jonas Bromö <jonas.bromo@gmail.com>
2024-05-24 09:50:08 +0200

    Make errors print a readable description when logged (not just a code)

--- Sources/SwiftTranslate/Models/SwiftTranslateError.swift
+++ Sources/SwiftTranslate/Models/SwiftTranslateError.swift
@@ -5,11 +5,29 @@
 import Foundation
 
 
-enum SwiftTranslateError: Error {
+enum SwiftTranslateError: LocalizedError {
     case couldNotSearchDirectoryAt(URL)
     case noTranslationReturned
     case unexpectedTranslationResponse
     case failedToParseTranslationResponse(String)
     case translationFailed
     case evaluationIsNotSupported
+
+    var errorDescription: String? {
+        switch self {
+        case .couldNotSearchDirectoryAt(let url):
+            "Could not search directory at: \(url)"
+        case .noTranslationReturned:
+            "No translation returned"
+        case .unexpectedTranslationResponse:
+            "Unexpected translation response"
+        case .failedToParseTranslationResponse(let message):
+            "Failed to parse translation response: \(message)"
+        case .translationFailed:
+            "Translation failed"
+        case .evaluationIsNotSupported:
+            "Evaluation is not supported"
+        }
+    }
+
 }
