c5e89ef81cbbd665ac47e8fc260f1e820e424bab
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2025-03-09 11:50:48 +0100

    Update to Gemini 2.0 Flash not experimental

--- Sources/SwiftTranslate/Models/AIModel.swift
+++ Sources/SwiftTranslate/Models/AIModel.swift
@@ -10,5 +10,5 @@ public enum AIModel: String, ExpressibleByArgument {
     case gpt3_5Turbo = "gpt-3.5-turbo"
     case gpt4o = "gpt-4o"
     case gemini15 = "gemini-1.5-flash"
-    case gemini20 = "gemini-2.0-flash-exp"
+    case gemini20 = "gemini-2.0-flash"
 }
