c2ffcc7854995c4b18e0ddecfd7cac5cf6554012
Andrew Theis <andrew@hiddenspectrum.io>
2026-07-28 10:45:26 -0500

    Add machine translated

--- Sources/SwiftStringCatalog/Models/TranslationState.swift
+++ Sources/SwiftStringCatalog/Models/TranslationState.swift
@@ -6,6 +6,7 @@ import Foundation
 
 
 public enum TranslationState: String, Codable, Equatable, Sendable {
+    case machineTranslated = "machine_translated"
     case new
     case needsReview = "needs_review"
     case stale
